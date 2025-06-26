877e4d2e8ee3ac3856001744adbd82af85d81e89
Thomas Lent <911786+tlent@users.noreply.github.com>
2025-06-26 16:51:47 -0400

    Add slightly optimized early return solution

--- arrays-and-hashing/contains-duplicate/solution.ts
+++ arrays-and-hashing/contains-duplicate/solution.ts
@@ -1,3 +1,12 @@
 function hasDuplicate(numbers: number[]): boolean {
   return (new Set(numbers)).size < numbers.length;
 }
+
+function hasDuplicateEarlyReturn(numbers: number[]): boolean {
+  const seen = new Set();
+  for (const number of numbers) {
+    if (seen.has(number)) return true;
+    seen.add(number);
+  }
+  return false;
+}
