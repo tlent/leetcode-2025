a28a34135b8626ba4d71f8d2cbd21c35453f3894
Thomas Lent <911786+tlent@users.noreply.github.com>
2025-06-27 16:21:04 -0400

    Format

--- python/src/sliding-window/best_time_to_buy_and_sell_stock.py
+++ python/src/sliding-window/best_time_to_buy_and_sell_stock.py
@@ -6,6 +6,7 @@ def max_profit(prices: list[int]) -> int:
         min_price = min(min_price, price)
     return max_profit
 
+
 def test_max_profit() -> None:
-    assert max_profit([7,1,5,3,6,4]) == 5
-    assert max_profit([7,6,4,3,1]) == 0
+    assert max_profit([7, 1, 5, 3, 6, 4]) == 5
+    assert max_profit([7, 6, 4, 3, 1]) == 0
