1ef5ea7adce57ea2343e566e1d1177fb1b9f6ba3
Thomas Lent <911786+tlent@users.noreply.github.com>
2025-06-27 16:37:35 -0400

    Format

--- typescript/src/sliding-window/best-time-to-buy-and-sell-stock.ts
+++ typescript/src/sliding-window/best-time-to-buy-and-sell-stock.ts
@@ -13,7 +13,7 @@ if (import.meta.vitest) {
   const { test, expect } = await import("vitest");
 
   test("maxProfit", () => {
-    expect(maxProfit([7,1,5,3,6,4])).toEqual(5);
-    expect(maxProfit([7,6,4,3,1])).toEqual(0);
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(5);
+    expect(maxProfit([7, 6, 4, 3, 1])).toEqual(0);
   });
 }
