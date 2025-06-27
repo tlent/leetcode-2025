12506c8b365b4251136d51b574ce244b146524a5
Thomas Lent <911786+tlent@users.noreply.github.com>
2025-06-27 07:42:43 -0400

    Simplify A charCode and remove unnecessary arrayEquals checks

--- arrays-and-hashing/valid-anagram/solution.ts
+++ arrays-and-hashing/valid-anagram/solution.ts
@@ -7,14 +7,12 @@ function isAnagram(s: string, t: string): boolean {
 function countCharacters(s: string): number[] {
   const counts = new Array(26).fill(0);
   for (const c of s) {
-    counts[c.charCodeAt(0) - "a".charCodeAt(0)] += 1;
+    counts[c.charCodeAt(0) - 97] += 1;
   }
   return counts;
 }
 
 function arrayEquals<Type>(a: Array<Type>, b: Array<Type>): boolean {
-  if (a === b) return true;
-  if (a == null || b == null || a.length !== b.length) return false;
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) return false;
   }
