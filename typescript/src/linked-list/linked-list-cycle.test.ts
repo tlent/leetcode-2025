2ef9e859e960cdbd899c86bcccbd629b1cb9a578
Thomas Lent <911786+tlent@users.noreply.github.com>
2025-06-29 13:09:14 -0400

    Move test utils used only in one test into test file

--- typescript/src/linked-list/linked-list-cycle.test.ts
+++ typescript/src/linked-list/linked-list-cycle.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "vitest";
 import { hasCycle } from "./linked-list-cycle";
-import { createCycleList } from "../test-utils";
+import { List, ListNode } from "./list";
 
 test("hasCycle example 1", () => {
   const list = createCycleList([3, 2, 0, -4], 1);
@@ -16,3 +16,17 @@ test("hasCycle example 3", () => {
   const list = createCycleList([1], -1);
   expect(hasCycle(list)).toBe(false);
 });
+
+function createCycleList(values: number[], cyclePos: number): List {
+  const list = ListNode.from_values(values);
+  if (cyclePos < 0 || !list) {
+    return list;
+  }
+
+  const nodes = Array.from(list.nodes());
+  if (cyclePos < nodes.length) {
+    nodes[nodes.length - 1]!.next = nodes[cyclePos]!;
+  }
+
+  return list;
+}
