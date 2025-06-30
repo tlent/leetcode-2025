035de21f382fe1142b740039ed2080c1e3fbe7db
Thomas Lent <911786+tlent@users.noreply.github.com>
2025-06-30 11:38:41 -0400

    Add invert_binary_tree_recursive

--- rust/src/trees/invert_binary_tree.rs
+++ rust/src/trees/invert_binary_tree.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use crate::trees::tree::TreeNode;
+use crate::trees::tree::{TreeNode};
 
 pub fn invert_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
     if root.is_none() {
@@ -19,6 +19,18 @@ pub fn invert_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefC
         if let Some(right) = &node.borrow().right {
             stack.push(right.clone());
         }
+    }
+    root
+}
+
+pub fn invert_binary_tree_recursive(
+    root: Option<Rc<RefCell<TreeNode>>>,
+) -> Option<Rc<RefCell<TreeNode>>> {
+    if let Some(node) = root.as_ref() {
+        let left = node.borrow_mut().left.take();
+        let right = node.borrow_mut().right.take();
+        node.borrow_mut().left = invert_binary_tree_recursive(right);
+        node.borrow_mut().right = invert_binary_tree_recursive(left);
     }
     root
 }
@@ -47,6 +59,27 @@ mod tests {
     fn test_invert_binary_tree_example_three() {
         let mut tree = Tree::new(&[]);
         tree.0 = invert_binary_tree(tree.0);
+        assert_eq!(tree.to_vec(), []);
+    }
+
+    #[test]
+    fn test_invert_binary_tree_recursive_example_one() {
+        let mut tree = Tree::new(&[4, 2, 7, 1, 3, 6, 9]);
+        tree.0 = invert_binary_tree_recursive(tree.0);
+        assert_eq!(tree.to_vec(), [4, 7, 2, 9, 6, 3, 1]);
+    }
+
+    #[test]
+    fn test_invert_binary_tree_recursive_example_two() {
+        let mut tree = Tree::new(&[2, 1, 3]);
+        tree.0 = invert_binary_tree_recursive(tree.0);
+        assert_eq!(tree.to_vec(), [2, 3, 1]);
+    }
+
+    #[test]
+    fn test_invert_binary_tree_recursive_example_three() {
+        let mut tree = Tree::new(&[]);
+        tree.0 = invert_binary_tree_recursive(tree.0);
         assert_eq!(tree.to_vec(), []);
     }
 }
