8c8459957f5aeb3f56cfbc481b1f6cab6237c519
Thomas Lent <911786+tlent@users.noreply.github.com>
2025-07-16 13:19:28 -0400

    Binary search for rust

--- rust/src/n0057_insert_interval.rs
+++ rust/src/n0057_insert_interval.rs
@@ -1,12 +1,22 @@
 pub fn insert(intervals: Vec<Vec<i32>>, mut new_interval: Vec<i32>) -> Vec<Vec<i32>> {
     let start = intervals
-        .iter()
-        .position(|interval| interval[1] >= new_interval[0])
-        .unwrap_or(intervals.len());
+        .binary_search_by(|interval| {
+            if interval[1] < new_interval[0] {
+                std::cmp::Ordering::Less
+            } else {
+                std::cmp::Ordering::Greater
+            }
+        })
+        .unwrap_or_else(|x| x);
     let end = intervals
-        .iter()
-        .position(|interval| interval[0] > new_interval[1])
-        .unwrap_or(intervals.len());
+        .binary_search_by(|interval| {
+            if interval[0] <= new_interval[1] {
+                std::cmp::Ordering::Less
+            } else {
+                std::cmp::Ordering::Greater
+            }
+        })
+        .unwrap_or_else(|x| x);
 
     if start < end {
         new_interval[0] = new_interval[0].min(intervals[start][0]);
